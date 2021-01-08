f5e32e9854c06f8ee9bae1f3321f893dcb60e451
Federico Capoano <federico.capoano@gmail.com>
2021-01-07 19:55:38 -0500

    [fix] Schedule openwisp-radius tasks only if radius enabled

--- templates/openwisp2/settings.py
+++ templates/openwisp2/settings.py
@@ -189,6 +189,7 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab(**{ {{ cron_delete_old_notifications }} }),
         'args': ({{ openwisp2_notifications_delete_old_notifications }},),
     },
+    {% if openwisp2_radius %}
     'deactivate_expired_users': {
         'task': 'openwisp_radius.tasks.cleanup_stale_radacct',
         'schedule': crontab(**{ {{ cron_deactivate_expired_users }} }),
@@ -219,6 +220,7 @@ CELERY_BEAT_SCHEDULE = {
         'args': [{{ openwisp2_radius_delete_old_radacct }}],
         'relative': True,
     },
+    {% endif %}
 }
 
 {% if openwisp2_celery_task_routes_defaults %}
